4669f4910633ddec4baa9663310975c5e5f0c190
Vanessa Cañete <vanecanhete@gmail.com>
2016-10-20 17:28:48 -0300

    Practica 3 del módulo 2

---
+++ modulo-2/practicas/practica-3/practica_clases.rb
@@ -0,0 +1,28 @@
+class Persona
+  #Persona debe tener atributos: nombre y apellido y deben ser accesibles
+  #Persona debe almacenar todas sus instancias para poder hacer seguimiento
+
+  #Inicializador de instancias
+  def initialize(x,y)#Debe recibir dos parametros: nombre y apellido
+
+  end
+
+  #La clase debe tener tener un search por apellido
+  def self.search(apellido)
+  end
+
+  #Tener un método que retorne el nombre y el apellido de la persona.
+  def to_s
+  end
+end
+
+dp1 = Persona.new("Juan", "Perez")
+p2 = Persona.new("Juan", "Benitez")
+p3 = Persona.new("Juana", "Perez")
+p4 = Persona.new("Romina", "Benitez")
+
+puts Persona.search("Perez")
+
+# Debería imprimir
+# => Juan Perez
+# => Juana Perez
