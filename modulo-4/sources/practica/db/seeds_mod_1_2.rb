683ade46598a928185b5577e4ec5b29342e34275
Vanessa Cañete <vanecanhete@gmail.com>
2016-11-22 23:33:16 -0300

    Preparación ejercicio modulo 5

---
+++ modulo-4/sources/practica/db/seeds_mod_1_2.rb
@@ -0,0 +1,51 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+
+User.create! [
+  { username: "Fiorina", password_digest: "Fiorina"},
+  { username: "Trump", password_digest: "Trump"},
+  { username: "Carson", password_digest: "Carson"},
+  { username: "Clinton", password_digest: "Clinton"}
+]
+
+
+User.find_by!(username: "Fiorina").create_profile(first_name: "Carly", last_name: "Fiorina", birth_year: 1954, gender: "female")
+User.find_by!(username: "Trump").create_profile(first_name: "Donald", last_name: "Trump", birth_year: 1946, gender: "male")
+User.find_by!(username: "Carson").create_profile(first_name: "Ben", last_name: "Carson", birth_year: 1951, gender: "male")
+User.find_by!(username: "Clinton").create_profile(first_name: "Hillary", last_name: "Clinton", birth_year: 1947, gender: "female")
+
+dateRange = 100
+dateTrack = Date.today
+User.find_by!(username: "Fiorina").todo_lists.create! [
+  { list_name: "List for Fiorina", list_due_date: dateTrack }
+]
+
+dateTrack = Date.today + rand(0..dateRange).day
+User.find_by!(username: "Trump").todo_lists.create! [
+  { list_name: "List for Trump", list_due_date: dateTrack }
+]
+
+dateTrack = Date.today + rand(0..dateRange).day
+User.find_by!(username: "Carson").todo_lists.create! [
+  { list_name: "List for Carson", list_due_date: dateTrack }
+]
+
+dateTrack = Date.today + rand(0..dateRange).day
+User.find_by!(username: "Clinton").todo_lists.create! [
+  { list_name: "List for Clinton", list_due_date: dateTrack }
+]
+
+TodoList.all.each do |td|
+  for i in 1..5 do
+      dateTrack = Date.today + rand(0..dateRange).day
+      td.todo_items.create!({ title: "Work #{i}", due_date: dateTrack,
+                              description: "Description #{i}", completed: false})
+    end
+  end
---
+++ modulo-5/sources/practica/db/migrate/20161119024619_add_user_to_todo_list.rb
@@ -0,0 +1,5 @@
+class AddUserToTodoList < ActiveRecord::Migration
+  def change
+    add_reference :todo_lists, :user, index: true, foreign_key: true
+  end
+end
