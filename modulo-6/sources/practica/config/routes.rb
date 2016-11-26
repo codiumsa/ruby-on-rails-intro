8c8cf72dfcc0c2c82a1376fa415ca2dfb8d633af
Vanessa Cañete <vanecanhete@gmail.com>
2016-11-25 23:09:44 -0300

    rq03

--- modulo-6/sources/practica/config/routes.rb
+++ modulo-6/sources/practica/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :todo_lists do
+    resources :todo_items
+  end
+
+  root to: "todo_lists#index"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
