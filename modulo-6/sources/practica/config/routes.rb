0aa631dcc8350cf5e3430f18e42bbc81a1d86bec
Vanessa Cañete <vanecanhete@gmail.com>
2016-11-26 01:16:13 -0300

    rq09

--- modulo-6/sources/practica/config/routes.rb
+++ modulo-6/sources/practica/config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   resources :todo_lists do
     resources :todo_items
   end
-
   root to: "todo_lists#index"
 
+  resources :sessions, only: [:new, :create, :destroy]
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
