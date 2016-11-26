d5adc65595d814037f29a54aa0fc072a9a123ac8
Vanessa Cañete <vanecanhete@gmail.com>
2016-11-26 01:26:39 -0300

    rq10c

--- modulo-6/sources/practica/app/controllers/sessions_controller.rb
+++ modulo-6/sources/practica/app/controllers/sessions_controller.rb
@@ -3,8 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:user][:username])
+    password = params[:user][:password]
+
+    if user && user.authenticate(password)
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in successfully"
+    else
+      redirect_to login_path, alert: "Invalid userusername/password combination"
+    end
   end
 
   def destroy
+    reset_session # wipe out session and everything in it
+    redirect_to login_path, notice: "You have been logged out"
   end
 end
