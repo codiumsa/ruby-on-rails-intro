620477dc21da29d1b954ff9229388364569cdea3
Vanessa Cañete <vanecanhete@gmail.com>
2016-11-26 02:05:17 -0300

    rq12

--- modulo-6/sources/practica/app/controllers/application_controller.rb
+++ modulo-6/sources/practica/app/controllers/application_controller.rb
@@ -19,6 +19,10 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id])
+    begin
+      @current_user ||= User.find(session[:user_id])
+    rescue Exception => e
+      reset_session
+    end
   end
 end
