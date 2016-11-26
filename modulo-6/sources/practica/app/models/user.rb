ca75a94fb924c9fe04c5f9f5de6d6ad5d831ea3e
Vanessa Cañete <vanecanhete@gmail.com>
2016-11-25 23:09:16 -0300

    rq02

--- modulo-6/sources/practica/app/models/user.rb
+++ modulo-6/sources/practica/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   has_one :profile, dependent: :destroy
   has_many :todo_lists, dependent: :destroy
   has_many :todo_items, through: :todo_lists, source: :todo_items
